187b9aa1ce3784f2e0a6dea471396c427851c81e
Majdi <nassib.haouari@gmail.com>
2019-03-18 20:29:31 +0100

    Adding @Test annotation, GivenWhenThen

--- src/test/java/com/priceminister/account/CustomerAccountTest.java
+++ src/test/java/com/priceminister/account/CustomerAccountTest.java
@@ -13,6 +13,7 @@ import com.priceminister.account.CustomerAccountTest;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Test;
 
 public class CustomerAccountTest extends AbstractTestCase {
 
@@ -31,32 +32,39 @@ public class CustomerAccountTest extends AbstractTestCase {
 		_customerAccount = new CustomerAccount();
 		_customerAccountRule = new CustomerAccountRule();
 	}
-
+	@Test
 	public void testAccountWithoutMoneyHasZeroBalance() throws IllegalBalanceException {
+		//Given
 		_customerAccount = new CustomerAccount("Legrand", new BigDecimal(0.00));
+		//When
+		//Then
 		assertEquals(_customerAccount.getBalance(), new BigDecimal(0.00));
 
 	}
-
+	@Test
 	public void testAddPositiveAmount() {
+		//Given
 		_customerAccount = new CustomerAccount("Client", new BigDecimal(100));
-		
+		//When
 		_customerAccount.add(new BigDecimal(200));
+		//Then
 		assertEquals(_customerAccount.getBalance(), new BigDecimal(300));
 
 	}
-
+	@Test
 	public void testWithdrawAndReportBalanceIllegalBalance() throws Exception {
+		//Given
 		BigDecimal widthDrawalAmount = new BigDecimal(150.0);
 
 		_customerAccount = new CustomerAccount("Legrand", new BigDecimal(500.00));
 		_customerAccountRule = new CustomerAccountRule();
-
+		//When
 		try {
 
 			if (_customerAccountRule.withdrawPermitted(_customerAccount.getBalance()))
 				_customerAccount.withdrawAndReportBalance(widthDrawalAmount, _customerAccountRule);
-			   assertEquals(_customerAccount.getBalance(), new BigDecimal(350.0));
+			//Then
+			assertEquals(_customerAccount.getBalance(), new BigDecimal(350.0));
 		} catch (IllegalBalanceException e) {
 			
 			fail("Compte non approvisionné");
